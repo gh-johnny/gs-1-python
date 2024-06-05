922fc24b41e1757e2ec0a88baace1793c6ac09a0
gh-johnny <jmfurtadoromero@gmail.com>
2024-06-04 23:59:01 -0300

    chore: adding simulation to main.py (app itself)

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-from helpers import forca_opcao, verifica_numero, meu_in, meu_index
+from helpers import forca_opcao, limpar_tela
+from sysfunctions import sys_dados, sys_doacao
+from simulacao import tela_simulacao
+
+nome_da_empresa = "Ocean Solution "
+lista_menu_opcao = ['1', '2', '3', '0']
+lista_praias = ["Maresias", "Juquehy", "Ubatuba"]
+
+while True:
+    limpar_tela()
+    print(f"Seja bem-vindo à {nome_da_empresa}!!!")
+    caminho = forca_opcao("Por qual caminho você deseja seguir:\n"
+                          "1 - Simulação\n"
+                          "2 - Dados capturados\n"
+                          "3 - Doação\n"
+                          "0 - Sair\n--> ", lista_menu_opcao, "Opção inválida! Digite apenas os números correspondentes ao caminho indicado")
+    limpar_tela()
+    if caminho == '1':
+        tela_simulacao()
+    elif caminho == '2':
+        print(f"Bem-vindo ao banco de dados {nome_da_empresa},\n aqui você encontrará todos os tipos de dados "
+              "capturados pelos nossos sensores oceânicos!\n")
+        sys_dados()
+    elif caminho == '3':
+        print(f"Bem-vindo a seção de doação {nome_da_empresa}, \naqui você pode fazer doações de quantias "
+              "de sua preferência para nos ajudar a manter os oceanos sempre monitorados e saudáveis!\n")
+        sys_doacao()
+    elif caminho == '0':
+        break
